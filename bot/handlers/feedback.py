e1274e5f649c69883341d4d46a2ae2f2d680b669
SRIKAR12344 <130546150+SRIKAR12344@users.noreply.github.com>
2023-04-15 17:49:04 +0530

    Update feedback.py

--- bot/handlers/feedback.py
+++ bot/handlers/feedback.py
@@ -11,7 +11,7 @@ logging.getLogger(__name__).setLevel(logging.INFO)
 OWNER_ID = vars.OWNER_ID
 
 LOG_TEXT = "ID: <code>{}</code>\nName: <a href='tg://user?id={}'>{}{}</a>\nStarted the bot..."
-START_TEXT = "You Can Give Feedback and Contact Admins by Sending Messages to Me..."
+START_TEXT = "𝗛𝗘𝗬 𝗪𝗔𝗦𝗦𝗨𝗣 𝗠𝗔𝗧𝗘 😍\n➜𝗪𝗘 𝗔𝗥𝗘 𝗧𝗛𝗘 𝗢𝗪𝗡𝗘𝗥𝗦 𝗢𝗙 𝗞𝗢𝗥𝗘𝗔𝗡 𝗗𝗥𝗔𝗠𝗔𝗦\n➜𝗛𝗢𝗣𝗘 𝗬𝗢𝗨 𝗔𝗥𝗘 𝗗𝗢𝗜𝗡𝗚 𝗪𝗘𝗟𝗟\n➜𝗦𝗢 𝗛𝗘𝗥𝗘 𝗪𝗘 𝗔𝗥𝗘 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗚𝗜𝗩𝗘 𝗙𝗘𝗘𝗗𝗕𝗔𝗖𝗞 𝗛𝗘𝗥𝗘 𝗔𝗕𝗢𝗨𝗧 𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n➜𝗥𝗘𝗤𝗨𝗘𝗦𝗧 𝗔𝗡𝗬 𝗞𝗗𝗥𝗔𝗠𝗔𝗦 𝗢𝗥 𝗠𝗢𝗩𝗜𝗘𝗦 𝗦𝗢 𝗪𝗘 𝗨𝗣𝗟𝗢𝗔𝗗 𝗜𝗧😇\n\n➜𝗥𝗨𝗟𝗘𝗦 𝗔𝗡𝗗 𝗖𝗢𝗡𝗗𝗜𝗧𝗜𝗢𝗡𝗦\n\n˚₊· ͟͟͞͞➳❥𝗣𝗟𝗦 𝗗𝗢𝗡𝗧 𝗦𝗣𝗔𝗠 𝗧𝗛𝗘 𝗕𝗢𝗧\n˚₊· ͟͟͞͞➳❥𝗝𝗨𝗦𝗧 𝗟𝗘𝗔𝗩𝗘 𝗧𝗛𝗘 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 𝗪𝗘 𝗪𝗜𝗟𝗟 𝗦𝗨𝗥𝗘𝗟𝗬 𝗥𝗘𝗣𝗟𝗬\n˚₊· ͟͟͞͞➳❥𝗗𝗢𝗡𝗧 𝗕𝗟𝗢𝗖𝗞 𝗧𝗛𝗘 𝗕𝗢𝗧 𝗕𝗬 𝗔𝗡𝗬 𝗠𝗘𝗔𝗡𝗦 𝗜𝗙 𝗬𝗢𝗨 𝗪𝗔𝗡𝗧 𝗥𝗘𝗣𝗟𝗬\n˚₊· ͟͟͞͞➳❥𝗝𝗨𝗦𝗧 𝗥𝗘𝗤𝗨𝗘𝗦𝗧 𝗧𝗛𝗘 𝗗𝗥𝗔𝗠𝗔𝗦 𝗢𝗥 𝗚𝗜𝗩𝗘 𝗙𝗘𝗘𝗗𝗕𝗔𝗖𝗞\n˚₊· ͟͟͞͞➳❥𝗘𝗡𝗝𝗢𝗬 𝗡𝗢𝗪 𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗝𝗢𝗜𝗡𝗜𝗡𝗚 𝗢𝗨𝗥 𝗖𝗛𝗔𝗡𝗡𝗘𝗟😍🥰"
 MESSAGE = "<b>Message from:</b> <code>{}</code>\n<b>Name:</b> <a href='tg://user?id={}'>{}{}</a>\n\n{}"
 
 
